8bcf2d2694f328645907004216cfd7ba252731ec
mastertope <tupengabs@gmail.com>
2022-01-04 15:54:37 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WireBrainCoffee.StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
